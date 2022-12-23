60c3bb2a1a5491d4223f34ad7051398cf59ccd9e
Elliot <ec2018@gmail.com>
2022-12-23 14:48:19 -0800

    Added deployment to whitelist

--- internal/RequestHandler.go
+++ internal/RequestHandler.go
@@ -19,9 +19,7 @@ type Request struct {
 
 var originWhitelist = []string{
 	"http://localhost:5173",
-	"http://localhost:5174",
-	"http://localhost:5175",
-	"http://localhost:5176",
+	"https://chatroom-frontend-one.vercel.app/",
 }
 
 var upgrader = websocket.Upgrader{
