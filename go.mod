6ddea5e537ac38512df87bdad8bcebee472c6ab4
Elliot <ec2018@gmail.com>
2022-12-23 04:01:25 -0800

    Envirornment variables and deployment code

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module chatroom
 
 go 1.19
 
-require github.com/gorilla/websocket v1.5.0
+require (
+	github.com/fsnotify/fsnotify v1.6.0
+	github.com/gorilla/websocket v1.5.0
+)
+
+require golang.org/x/sys v0.0.0-20220908164124-27713097b956 // indirect
--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"chatroom/internal"
+	"fmt"
 	"log"
 	"net/http"
-)
+	"os"
+	"strings"
 
-const (
-	CONN_HOST = ""
-	CONN_PORT = "443"
+	"github.com/fsnotify/fsnotify"
 )
 
 func main() {
@@ -20,8 +20,42 @@ func main() {
 		internal.RequestHandler(canvas, bh, w, r)
 	})
 
-	log.Println("Listening on " + CONN_HOST + ":" + CONN_PORT + "...")
-	err := http.ListenAndServe(CONN_HOST+":"+CONN_PORT, nil)
+	// creates a new file watcher for App_offline.htm
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		fmt.Println("ERROR", err)
+	}
+	defer watcher.Close()
+
+	// watch for App_offline.htm and exit the program if present
+	// This allows continuous deployment on App Service as the .exe will not be
+	// terminated otherwise
+	go func() {
+		for event := range watcher.Events {
+			if strings.HasSuffix(event.Name, "app_offline.htm") {
+				fmt.Println("Exiting due to app_offline.htm being present")
+				os.Exit(0)
+			}
+		}
+	}()
+
+	// get the current working directory and watch it
+	currentDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("ERROR", err)
+	}
+
+	if err := watcher.Add(currentDir); err != nil {
+		fmt.Println("ERROR", err)
+	}
+
+	port := os.Getenv("SERVER_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Println("Listening on " + port)
+	err = http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
