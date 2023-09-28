fc0f14f679b658f1c03c283deba2eb15dcb3143e
Elliot <ec2018@gmail.com>
2023-09-27 21:07:11 -0700

    configuration for deploying using fly

--- main.go
+++ main.go
@@ -5,9 +5,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strings"
-
-	"github.com/fsnotify/fsnotify"
 )
 
 func main() {
@@ -25,44 +22,13 @@ func main() {
 		internal.RequestHandler(canvas, bh, w, r)
 	})
 
-	// creates a new file watcher for App_offline.htm
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Println("ERROR", err)
-	}
-	defer watcher.Close()
-
-	// watch for App_offline.htm and exit the program if present
-	// This allows continuous deployment on App Service as the .exe will not be
-	// terminated otherwise
-	go func() {
-		for event := range watcher.Events {
-			if strings.HasSuffix(event.Name, "app_offline.htm") {
-				log.Println("Exiting due to app_offline.htm being present")
-				os.Exit(0)
-			}
-		}
-	}()
-
-	// get the current working directory and watch it
-	currentDir, err := os.Getwd()
-	if err != nil {
-		log.Println("ERROR", err)
-	}
-
-	if err := watcher.Add(currentDir); err != nil {
-		log.Println("ERROR", err)
-	}
-
-	// Azure env variables
-	hostname := os.Getenv("WEBSITE_HOSTNAME")
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
-	log.Println("Listening on " + hostname + ":" + port)
-	err = http.ListenAndServe(":"+port, nil)
+	log.Println("Listening on " + port)
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
